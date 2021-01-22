9a1acff57a6254bf355a704150101a78f7502ea6
HR Williams <halrubinwilliams@gmail.com>
2021-01-22 11:45:33 -0800

    Comment out UI logic, start console loop which starts first test

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
-$(document).ready(function()) {
-  $("form#mr-robogers").submit(function(event) {
-    event.preventDefault();
-    var userInput = $("input#number").val()
-    $("#result").text(beepBoop(userInput));
-    $("#result").show();
-  });
-});
+// $(document).ready(function()) {
+//   $("form#mr-robogers").submit(function(event) {
+//     event.preventDefault();
+//     var userInput = $("input#number").val()
+//     $("#result").text(beepBoop(userInput));
+//     $("#result").show();
+//   });
+// });
+
+let number = 0
+for (let index = 0; index <= number; index++) {
+  console.log(index)
+}
